cc2f6dd231c3d7387eaa28f9b6cbdc50cb256d7f
Murm <murmiration@gmail.com>
2022-05-22 09:59:40 -0400

    added invite command

---
+++ commands/general/invite.js
@@ -0,0 +1,10 @@
+import Command from "../../classes/command.js";
+
+class InviteCommand extends Command {
+    async run() {
+        const placeholder = "https://app.revolt.chat/bot/01G3K8ZEKXF86QEDJYNCEBT492";
+        return placeholder;
+    }
+}
+
+export default InviteCommand;
