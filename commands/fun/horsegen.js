957523c363c2d1508cec17b705be6cebc4dbbc50
Murm <murmiration@gmail.com>
2022-05-21 22:54:18 -0400

    a certified I forgot to do something moment

--- commands/fun/horsegen.js
+++ commands/fun/horsegen.js
@@ -3,7 +3,6 @@ import Command from "../../classes/command.js";
 
 class HorseCommand extends Command {
   async run() {
-    this.client.sendChannelTyping(this.message.channel.id);
     const request = await fetch("https://thishorsedoesnotexist.com");
     return {
       file: await request.buffer(),
